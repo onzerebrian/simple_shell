f521f48b913feb7bcb6de7302b0e5315d86329ad
Engefu <57341697+Engefu@users.noreply.github.com>
2023-05-25 03:47:26 +0300

    Update _strtok.c

--- _strtok.c
+++ _strtok.c
@@ -1,7 +1,6 @@
 #include "main.h"
-
 /**
- * check_delim- Checks If A Character Match Any Char 
+ * check_delim - Checks If A Character Match Any Char *
  * @c: Character To Check
  * @str: String To Check
  * Return: 1 Succes, 0 Failed
@@ -19,7 +18,7 @@ unsigned int check_delim(char c, const char *str)
 }
 
 /**
- * _strtok - Tokenize A String Into Token (strtrok)
+ * _strtok - Token A String Into Token (strtrok)
  * @str: String
  * @delim: Delimiter
  * Return: Pointer To The Next Token Or NULL
